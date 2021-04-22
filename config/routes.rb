7da69c05b14130e5433bdcddef43dc11fee5e1ed
Chirag.me.dev <chirag.me.dev@gmail.com>
2021-04-23 02:36:27 +0530

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :places
   resources :users
   resources :companies
-  root 'home#index'
+  root 'companies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
